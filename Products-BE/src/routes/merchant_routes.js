d81eacbdbad14af5d84ee63518c8066d533a084c
AnthonyIshakk <Anthonyishak1000@gmail.com>
2025-09-29 09:55:32 +0300

    Delete Products-BE directory

--- Products-BE/src/routes/merchant_routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { listMerchants } from "../controllers/merchant_controller.js";
-import { requireAuth } from "../middleware/user_auth.js";
-
-const merchantRouter = Router();
-
-merchantRouter.get("/", requireAuth, listMerchants);
-
-export default merchantRouter; 
